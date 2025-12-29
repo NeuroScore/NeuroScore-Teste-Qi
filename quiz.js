563d1e619617a37e63a8df908299efa2aac2c5c6
Dan13l23x <danielbombasseiam@gmail.com>
2025-12-29 02:19:10 -0300

    asa

--- quiz.js
+++
@@ -1,9 +0,0 @@
-function finalizar() {
-  let score = 0;
-  document.querySelectorAll("input:checked")
-    .forEach(i => score += parseInt(i.value));
-
-  localStorage.setItem("score", score);
-
-  window.location.href = "pagamento.html";
-}
